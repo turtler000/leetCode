0c3f7e1452c0065cc215e5f1c9f1e9f15cba1f07
hanhuanyu <hanhuanyu@inspur.com>
2020-06-30 18:03:00 +0800

    feat: questionthree

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -3,6 +3,8 @@ package org;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.leetCode.questionthree.Solution.lengthOfLongestSubstring;
+
 /**
  * spring-boot startup
  *
@@ -11,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application
 {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        String a ="tab";
+        System.out.println(lengthOfLongestSubstring(a));
     }
 }
