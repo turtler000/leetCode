bccc510a4d615eec4ad49b61c4f3ac6468cc7289
hanhuanyu <hanhuanyu@inspur.com>
2020-07-01 20:31:33 +0800

    feat: add question two solution

--- src/main/java/org/application/Application.java
+++ src/main/java/org/application/Application.java
@@ -16,13 +16,13 @@ public class Application
 {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        ListNode l1=new ListNode(2);
-        l1.setNext(new ListNode(4));
-        l1.getNext().setNext(new ListNode(3));
-        ListNode l2=new ListNode(5);
-        l2.setNext(new ListNode(6));
-        l2.getNext().setNext(new ListNode(4));
-        ListNode l3=addTwoNumbers(l1,l2,0);
+        ListNode l1=new ListNode(1);
+        l1.setNext(new ListNode(8));
+//        l1.getNext().setNext(new ListNode(3));
+        ListNode l2=new ListNode(0);
+//        l2.setNext(new ListNode(6));
+//        l2.getNext().setNext(new ListNode(4));
+        ListNode l3=addTwoNumbers(l1,l2);
        print(l3);
     }
 
--- src/main/java/org/leetCode/questiontwo/Solution.java
+++ src/main/java/org/leetCode/questiontwo/Solution.java
@@ -3,16 +3,23 @@ package org.leetCode.questiontwo;
 import org.leetCode.questiontwo.ListNode;
 
 public class Solution {
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2,int n) {
-        if(l1==null||l2==null){
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        return addTwoNumbers_1(l1,l2,0);
+    }
+    public static  ListNode addTwoNumbers_1(ListNode l1, ListNode l2,int n) {
+        if(l1==null&&l2==null){
             if(n!=0) {
                 ListNode list = new ListNode(n);
                 return list;
             }
             return null;
-        }else {
+        }else if(l1==null&&l2!=null){
+            l1 =new ListNode(0);
+        }else if(l1!=null&&l2==null){
+            l2 =new ListNode(0);
+        }
             int ans = l1.val + l2.val + n;
-            ListNode list = new ListNode(n);
+            ListNode list ;
             if (ans >= 10) {
                 list = new ListNode(ans - 10);
                 n = 1;
@@ -20,13 +27,12 @@ public class Solution {
                 list = new ListNode(ans);
                 n = 0;
             }
-            if (l2.next != null && l2.next != null) {
-                list.next = addTwoNumbers(l1.next, l2.next, n);
+            if (l1.next != null || l2.next != null) {
+                list.next = addTwoNumbers_1(l1.next, l2.next, n);
             }else if(n!=0){
-                list.next=addTwoNumbers(null,null,n);
+                list.next=addTwoNumbers_1(null,null,n);
             }
             return list;
-        }
     }
     public static void print(ListNode listNode){
         System.out.println(listNode.getVal());
