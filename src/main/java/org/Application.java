194022ee368ab641723ac154196d843e5c80dcdd
hanhuanyu <hanhuanyu@inspur.com>
2020-09-05 13:40:42 +0800

    feat: 新增问题五
    此方法超时

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -2,19 +2,19 @@ package org;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+
+import static org.leetCode.questionfive.Questionfive.getPalindromeList;
+import static org.leetCode.questionfive.Questionfive.longestPalindrome;
 import static org.leetCode.questionseven.QuestionSeven.reverse;
 
 /**
  * spring-boot startup
- *
  */
 @SpringBootApplication
-public class Application
-{
+public class Application {
     public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-        int a[]={1,3,4,5,9,11};
-        System.out.println(reverse(12432532));
-
+String a = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+System.out.println(longestPalindrome(a));
     }
 }
--- src/main/java/org/leetCode/questionfive/Questionfive.java
+++ src/main/java/org/leetCode/questionfive/Questionfive.java
@@ -1,5 +1,10 @@
 package org.leetCode.questionfive;
 
+import org.apache.tomcat.util.buf.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
  * <p>
@@ -21,10 +26,67 @@ package org.leetCode.questionfive;
  */
 public class Questionfive {
     public static String longestPalindrome(String s) {
+        if("".equals(s)){
+            return "";
+        }
         char[] a = s.toCharArray();
-        int al = a.length;
-        char[] ret = new char[al];
-        return null;
+        int aLength = a.length;
+        List<Character> ret = new ArrayList();
+        List<Character> temp = new ArrayList();
+        //左循环
+        for (int i = 0; i < aLength; i++) {
+            int left = i;
+            //右循环
+            for (int right = aLength - 1; right > left; right--) {
+                int lefttag = left;
+                int righttag = right;
+                //若相等，进入左右相遇循环
+                while (a[lefttag] == a[righttag]) {
+                    temp.add(a[lefttag]);
+                    //左右为一个，奇数回文
+                    if (righttag - lefttag == 0) {
+                        temp = getPalindromeList(temp, 1);
+                        if (ret == null || ret.size() <= temp.size()) {
+                            ret.clear();
+                            ret.addAll(temp);
+                        }
+                        break;
+                    } else if (righttag - lefttag == 1) {
+                        temp = getPalindromeList(temp, 0);
+                        if (ret == null || ret.size() <= temp.size()) {
+                            ret.clear();
+                            ret.addAll(temp);
+                        }
+                        break;
+                    }
+                    lefttag++;
+                    righttag--;
+                }
+                temp.clear();
+            }
+
+        }
+        StringBuilder retS = new StringBuilder();
+        for (char item : ret) {
+            retS.append(item);
+        }
+        return retS.toString();
+    }
+
+    public static List getPalindromeList(List list, int tag) {
+        //偶数
+        if (tag == 0) {
+            for (int i = list.size() - 1; i >= 0; i--) {
+                list.add(list.get(i));
+            }
+        }
+        //奇数
+        if (tag == 1) {
+            for (int i = list.size() - 2; i >= 0; i--) {
+                list.add(list.get(i));
+            }
+        }
+        return list;
     }
 }
 
