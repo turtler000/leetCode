a4e3c8df13b1e7d820512225b2b1dc7f45dfeb93
hanhuanyu <hanhuanyu@inspur.com>
2020-10-31 11:53:13 +0800

    fix: 修改第八题，不需要每次都遍历栈

--- src/main/java/org/sword/offereight/CQueue.java
+++ src/main/java/org/sword/offereight/CQueue.java
@@ -1,7 +1,6 @@
 package org.sword.offereight;
 
 import java.util.LinkedList;
-import java.util.Stack;
 
 /**
  * create by hanhuanyu ON 2020/10/31
@@ -9,35 +8,39 @@ import java.util.Stack;
  * CQueue obj = new CQueue();
  * obj.appendTail(value);
  * int param_2 = obj.deleteHead();
- *
- *
+ * <p>
+ * <p>
  * 头栈负责删除，尾栈负责新增，每次操作都清空一个栈(每次都倒腾累不累啊，待优化）
  */
 public class CQueue {
-   private LinkedList<Integer> stackHead ;
-    private LinkedList<Integer> stackTail ;
+    private LinkedList<Integer> stackHead;
+    private LinkedList<Integer> stackTail;
+
     public CQueue() {
         this.stackHead = new LinkedList();
         this.stackTail = new LinkedList();
 
     }
+
     public void appendTail(int value) {
         //遍历头栈，放到尾栈里
-          while(stackHead.size()!=0){
-              stackTail.push(stackHead.pop());
-          }
-          stackTail.push(value);
+//          while(stackHead.size()!=0){
+//              stackTail.push(stackHead.pop());
+//          }
+        stackTail.push(value);
     }
 
     public int deleteHead() {
-      //空队列返回-1
-        if (stackHead.size()==0&&stackTail.size()==0){
+        //空队列返回-1
+        if (stackTail.isEmpty() && stackHead.isEmpty()) {
             return -1;
         }
-        //遍历尾栈，放到头栈里
-            while(stackTail.size()!=0){
+        //若头栈为空，遍历尾栈，放到头栈里
+        if(stackHead.isEmpty()) {
+            while (!stackTail.isEmpty()) {
                 stackHead.push(stackTail.pop());
             }
-        return (int)stackHead.pop();
+        }
+        return stackHead.pop();
     }
 }
