4b48a6b03ddc6407826d7634961c24d895739d50
hanhuanyu <hanhuanyu@inspur.com>
2020-11-07 18:23:41 +0800

    feat: 新增十二题，todo 深拷贝

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -1,7 +1,10 @@
 package org;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.sword.offereleven.MinArray;
 import org.sword.offerten.OfferTenA;
+import org.sword.offerten.OfferTenB;
+import org.sword.offertwelve.Exist;
 
 /**
  * spring-boot startup
@@ -9,9 +12,10 @@ import org.sword.offerten.OfferTenA;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
-System.out.println(OfferTenA.fib(99));
-  //      System.out.println(OfferTenA.fib_1(50));
-
+      char[][]  board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+      String  word = "ABCCED";
+      char[][] board1 ={{'C','A','A'},{'A','A','A'},{'B','C','D'}};
+      String word1= "AAB";
+System.out.println(Exist.exist_1(board1,word1));
     }
 }
