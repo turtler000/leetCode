1ed61e57baa359ab1f4c25f1a388a919902d670d
hanhuanyu <hanhuanyu@inspur.com>
2020-12-01 20:03:06 +0800

    feat: 新增十三题，没提交

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -1,10 +1,7 @@
 package org;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.sword.offereleven.MinArray;
-import org.sword.offerten.OfferTenA;
-import org.sword.offerten.OfferTenB;
-import org.sword.offertwelve.Exist;
+import org.sword.offerthirteen.OfferThirteen;
 
 /**
  * spring-boot startup
@@ -12,11 +9,8 @@ import org.sword.offertwelve.Exist;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-      char[][]  board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
-      String  word = "ABCCED";
-      char[][] board1 ={{'C','A','A'},{'A','A','A'},{'B','C','D'}};
-      String word1= "AAB";
-System.out.println(Exist.exist_1(board1,word1));
+        int a = 739;
+        System.out.println(OfferThirteen.movingCount(3,1,0));
 
     }
 }
