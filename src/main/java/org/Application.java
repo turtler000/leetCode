8685e0eeca2b11317e0b5559c7770cf829c58854
hanhuanyu <hanhuanyu@inspur.com>
2020-12-04 17:18:21 +0800

    feat: 新增17题

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.sword.offerfifteen.OfferFifteen;
 import org.sword.offerfourteen.OfferFourteen;
 import org.sword.offerfourteen.OfferFourteenExt;
+import org.sword.offerseventeen.OfferSeventeen;
 import org.sword.offersixteen.OfferSixteen;
 import org.sword.offerthirteen.OfferThirteen;
 
@@ -14,8 +15,10 @@ import org.sword.offerthirteen.OfferThirteen;
 public class Application {
     public static void main(String[] args) {
 
-      System.out.println(OfferSixteen.myPow(3,-4));
-      double a =0.012345679012345678;
-      System.out.println(1/a);
+//      System.out.println(OfferSixteen.myPow(3,-4));
+//      double a =0.012345679012345678;
+//      System.out.println(1/a);
+        int[] ret = OfferSeventeen.printNumbers(1);
+        System.out.println(ret.length);
     }
 }
