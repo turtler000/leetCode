8db4f89280c75d839649c2b342255a4307115c12
hanhuanyu <hanhuanyu@inspur.com>
2020-12-04 11:26:01 +0800

    feat: 新增15题

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -1,6 +1,7 @@
 package org;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.sword.offerfifteen.OfferFifteen;
 import org.sword.offerfourteen.OfferFourteen;
 import org.sword.offerfourteen.OfferFourteenExt;
 import org.sword.offerthirteen.OfferThirteen;
@@ -11,8 +12,8 @@ import org.sword.offerthirteen.OfferThirteen;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println(OfferFourteen.cuttingRope(100));
-        System.out.println(OfferFourteenExt.cuttingRope(100));
-        System.out.println(7412080755407364L%1000000007);
+String a = "11111111111111111111111111111101";
+System.out.println(Integer.valueOf(a,10));
+      System.out.println(OfferFifteen.hammingWeight(8));
     }
 }
