a39847df100f8adece197e3b120f5d26ae6c96cd
hanhuanyu <hanhuanyu@inspur.com>
2020-12-10 19:54:01 +0800

    feat: 新增部分题解

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -2,7 +2,7 @@ package org;
 
 import org.common.ListNode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.sword.offer_24.Solution;
+import org.sword.offer_25.Solution;
 
 
 /**
@@ -11,12 +11,17 @@ import org.sword.offer_24.Solution;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-ListNode l = new ListNode(5);
-l.next=new ListNode(4);
-l.next.next=new ListNode(3);
-l.next.next.next=new ListNode(2);
-l.next.next.next.next = new ListNode(1);
-ListNode s = Solution.reverseList(l);
-System.out.println(s.val);
+        ListNode l = new ListNode(1);
+        l.next = new ListNode(3);
+        l.next.next = new ListNode(4);
+        l.next.next.next = new ListNode(7);
+        l.next.next.next.next = new ListNode(10);
+
+        ListNode l2 = new ListNode(4);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(7);
+        l2.next.next.next = new ListNode(8);
+        ListNode s = Solution.mergeTwoLists(l,l2);
+        System.out.println(s.val);
     }
 }
