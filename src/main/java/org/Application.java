45cfc396da90a2d762a5875e87b30b405e4910e8
hanhuanyu <hanhuanyu@inspur.com>
2020-12-24 20:41:50 +0800

    feat: 27,一遍就过贼牛逼

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -1,9 +1,8 @@
 package org;
 
-import org.common.ListNode;
 import org.common.TreeNode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.sword.offer_26.Solution;
+import org.sword.offer_27.Solution;
 
 
 /**
@@ -12,16 +11,14 @@ import org.sword.offer_26.Solution;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-TreeNode A = new TreeNode(1);
-A.left = new TreeNode(2);
-A.right = new TreeNode(3);
-A.left.right = new TreeNode(4);
-//A.right.left = new TreeNode(1);
-//A.right.right = new TreeNode(0);
-TreeNode B = new TreeNode(3);
-//B.left = new TreeNode(1);
-boolean x   = Solution.isSubStructure_0(A,null);
-System.out.println(x);
+        TreeNode A = new TreeNode(1);
+        A.left = new TreeNode(2);
+        A.right = new TreeNode(3);
+        A.left.right = new TreeNode(4);
+        A.right.left = new TreeNode(1);
+        A.right.right = new TreeNode(0);
+        TreeNode x = Solution.mirrorTree(A);
+        System.out.println(x);
     }
 }
 
