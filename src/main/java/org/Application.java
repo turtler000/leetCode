ecfbd34f4224411a73be1649b165805981fbcc3b
hanhuanyu <hanhuanyu@inspur.com>
2021-01-11 14:37:03 +0800

    refactor: rename package name

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -14,10 +14,14 @@ import org.weekends.week_223.Solution_3;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-int[] source = { 1,2,3,4};
-int[] target = {2,1,4,5};
+int[] source = {5,1,2,4,3};
+int[] target = {1,5,4,2,3};
+
+//[5,1,2,4,3]
+//[1,5,4,2,3]
+//[[0,4],[4,2],[1,3],[1,4]]
 int[][] allowedSwaps = {{0,1},{2,3}};
-int ret = Solution_3.minimumHammingDistance(source,target,allowedSwaps);
+int ret = Solution_3.minimumHammingDistance_1(source,target,allowedSwaps);
 System.out.println(11);
     }
 
--- src/main/java/org/weekends/week_223/Solution_3.java
+++ src/main/java/org/weekends/week_223/Solution_3.java
@@ -8,11 +8,16 @@ import java.util.Map;
 /**
  * create by dell ON 2021/1/10
  * <p>
- * 给你两个整数数组 source 和 target ，长度都是 n 。还有一个数组 allowedSwaps ，其中每个 allowedSwaps[i] = [ai, bi] 表示你可以交换数组 source 中下标为 ai 和 bi（下标从 0 开始）的两个元素。注意，你可以按 任意 顺序 多次 交换一对特定下标指向的元素。
+ * 给你两个整数数组 source 和 target ，长度都是 n 。还有一个数组 allowedSwaps ，
+ * 其中每个 allowedSwaps[i] = [ai, bi] 表示你可以交换数组 source 中下标为 ai 和 bi（下标从 0 开始）
+ * 的两个元素。注意，你可以按 任意 顺序 多次 交换一对特定下标指向的元素。
  * <p>
- * 相同长度的两个数组 source 和 target 间的 汉明距离 是元素不同的下标数量。形式上，其值等于满足 source[i] != target[i] （下标从 0 开始）的下标 i（0 <= i <= n-1）的数量。
+ * 相同长度的两个数组 source 和 target 间的 汉明距离 是元素不同的下标数量。
+ * 形式上，其值等于满足 source[i] != target[i] （下标从 0 开始）的下标 i（0 <= i <= n-1）的数量。
  * <p>
  * 在对数组 source 执行 任意 数量的交换操作后，返回 source 和 target 间的 最小汉明距离 。
+ *
+ * todo 使用并查集
  */
 public class Solution_3 {
 
@@ -50,6 +55,24 @@ public class Solution_3 {
                 if (source[i] != target[i]) {
                     ret++;
                 }
+            }
+        }
+        return ret;
+    }
+    public static int minimumHammingDistance_1(int[] source, int[] target, int[][] allowedSwaps){
+        int ret = 0;
+        for(int i =0;i<allowedSwaps.length;i++){
+            int a = allowedSwaps[i][0];
+            int b = allowedSwaps[i][1];
+            if(source[a]==target[b]||source[b]==target[a]){
+                int temp = source[a];
+                source[a] = source[b];
+                source[b] = temp;
+            }
+        }
+        for(int i = 0;i<target.length;i++){
+            if(source[i]!=target[i]){
+                ret++;
             }
         }
         return ret;
