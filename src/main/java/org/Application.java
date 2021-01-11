5beef3a0adec048535daf8ac2022cdb843151e14
hanhuanyu <hanhuanyu@inspur.com>
2021-01-11 16:42:19 +0800

    feat: 剑指31题

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -3,7 +3,7 @@ package org;
 import org.common.ListNode;
 import org.common.TreeNode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.weekends.week_223.Solution;
+import org.sword.offer_31.Solution;
 import org.weekends.week_223.Solution_2;
 import org.weekends.week_223.Solution_3;
 
@@ -14,14 +14,9 @@ import org.weekends.week_223.Solution_3;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-int[] source = {5,1,2,4,3};
-int[] target = {1,5,4,2,3};
-
-//[5,1,2,4,3]
-//[1,5,4,2,3]
-//[[0,4],[4,2],[1,3],[1,4]]
-int[][] allowedSwaps = {{0,1},{2,3}};
-int ret = Solution_3.minimumHammingDistance_1(source,target,allowedSwaps);
+int[] pushed = {1,0};
+int[] poped = {1,0};
+boolean boo = Solution.validateStackSequences(pushed,poped);
 System.out.println(11);
     }
 
