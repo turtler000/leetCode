51960c15c4f8cd30add9c026b648c9654048a9e3
hanhuanyu <hanhuanyu@inspur.com>
2021-01-14 15:53:48 +0800

    feat: 剑指32题

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -3,9 +3,8 @@ package org;
 import org.common.ListNode;
 import org.common.TreeNode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.sword.offer_31.Solution;
-import org.weekends.week_223.Solution_2;
-import org.weekends.week_223.Solution_3;
+import org.sword.offer_32.Solution;
+
 
 
 /**
@@ -14,10 +13,12 @@ import org.weekends.week_223.Solution_3;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-int[] pushed = {1,0};
-int[] poped = {1,0};
-boolean boo = Solution.validateStackSequences(pushed,poped);
-System.out.println(11);
+TreeNode a = new TreeNode(1);
+a.right = new TreeNode(2);
+a.right.left = new TreeNode(9);
+        a.right.right = new TreeNode(4);
+        int[] ret = Solution.levelOrder(a);
+        System.out.println(ret);
     }
 
 }
