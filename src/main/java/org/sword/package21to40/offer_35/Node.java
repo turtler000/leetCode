8e8f005890c29fbe8c773ce0f6bb70756d7694c2
hanhuanyu <hanhuanyu@inspur.com>
2021-01-16 15:36:03 +0800

    feat: 剑指35题

---
+++ src/main/java/org/sword/package21to40/offer_35/Node.java
@@ -0,0 +1,16 @@
+package org.sword.package21to40.offer_35;
+
+/**
+ * create by hanhuanyu ON 2021/1/16
+ */
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
