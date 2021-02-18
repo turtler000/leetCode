4fcf8cb4c34b2b605eaf26da7f24996bb7caa2c1
hanhuanyu <hanhuanyu@inspur.com>
2021-02-18 22:27:45 +0800

    feat: offer_39

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -3,7 +3,7 @@ package org;
 import org.common.ListNode;
 import org.common.TreeNode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.sword.package21to40.offer_37.Solution;
+import org.sword.package21to40.offer_39.Solution;
 
 
 import java.util.List;
@@ -15,12 +15,8 @@ import java.util.List;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        TreeNode node = new TreeNode(4);
-node.left = new TreeNode(2);
-node.right = new TreeNode(5);
-node.left.left = new TreeNode(1);
-node.left.right = new TreeNode(3);
-String ret = Solution.serialize(node);
+String s = "aabv";
+String[] ret = Solution.permutation(s);
 System.out.println(ret);
     }
 
