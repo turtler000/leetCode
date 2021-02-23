157eb96f34385edc0af4b4644ec6e78886515c85
hanhuanyu <hanhuanyu@inspur.com>
2021-02-23 20:30:04 +0800

    refactor: refactor package name

--- src/main/java/org/sword/package21to40/offer_38/Solution.java
+++ src/main/java/org/sword/package21to40/offer_38/Solution.java
@@ -1,9 +1,7 @@
-package org.sword.package21to40.offer_39;
+package org.sword.package21to40.offer_38;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * 剑指 Offer 38. 字符串的排列
