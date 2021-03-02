43d2a37e35ed6d7e479787574c9def7236d7c464
hanhuanyu <hanhuanyu@inspur.com>
2021-03-02 22:15:20 +0800

    feat: 46题，还是得多练

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -1,7 +1,7 @@
 package org;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.sword.package41to60.offer_42.Solution;
+import org.sword.package41to60.offer_46.Solution;
 
 
 /**
@@ -10,10 +10,11 @@ import org.sword.package41to60.offer_42.Solution;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-int[] a = {-2,1,-3,4,-1,2,1,-5,4};
-int aa = Solution.maxSubArray(a);
-System.out.println(aa);
+int a = 12258;
+int b = 1225;
+int s = Solution.translateNum(a);
+System.out.println(s);
     }
-
+// 12 22 25 1225
 }
 
---
+++ src/main/java/org/sword/package41to60/offer_44/Solution.java
@@ -0,0 +1,8 @@
+package org.sword.package41to60.offer_44;
+
+/**
+ * @Author: hanhuanyu
+ * @Date: 2021/3/2 10:20
+ */
+public class Solution {
+}
