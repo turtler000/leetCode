44e543a0643bd3e3ab20a1c6accf3bdbadf29b9a
hanhuanyu <hanhuanyu@inspur.com>
2021-03-02 10:03:41 +0800

    feat: 41题超时

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -2,6 +2,11 @@ package org;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.sword.package21to40.offer_40.Solution;
+import org.sword.package41to60.offer_41.MedianFinder;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 
 /**
@@ -10,9 +15,27 @@ import org.sword.package21to40.offer_40.Solution;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        int[] a = {0,0,1,2,4,2,2,3,1,4};
-        int[] aa = Solution.getLeastNumbers(a,8);
-        System.out.println(aa);
+        MedianFinder medianFinder = new MedianFinder();
+        medianFinder.addNum(12);
+        medianFinder.addNum(10);
+        medianFinder.addNum(13);
+        medianFinder.addNum(11);
+        medianFinder.addNum(5);
+        medianFinder.addNum(15);
+        medianFinder.addNum(1);
+        medianFinder.addNum(11);
+        medianFinder.addNum(6);
+        medianFinder.addNum(17);
+        medianFinder.addNum(14);
+        medianFinder.addNum(8);
+        medianFinder.addNum(17);
+        medianFinder.addNum(6);
+        medianFinder.addNum(4);
+        medianFinder.addNum(16);
+        medianFinder.addNum(8);
+        medianFinder.addNum(10);
+        double a = medianFinder.findMedian();
+        System.out.println(a);
     }
 
 }
