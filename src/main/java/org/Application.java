0a41a2aa36e129f4bc75f63567251a11fa17a9f1
hanhuanyu <hanhuanyu@inspur.com>
2021-03-02 10:16:24 +0800

    feat: 42题，动态规划

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -1,12 +1,7 @@
 package org;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.sword.package21to40.offer_40.Solution;
-import org.sword.package41to60.offer_41.MedianFinder;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import org.sword.package41to60.offer_42.Solution;
 
 
 /**
@@ -15,27 +10,9 @@ import java.util.List;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        MedianFinder medianFinder = new MedianFinder();
-        medianFinder.addNum(12);
-        medianFinder.addNum(10);
-        medianFinder.addNum(13);
-        medianFinder.addNum(11);
-        medianFinder.addNum(5);
-        medianFinder.addNum(15);
-        medianFinder.addNum(1);
-        medianFinder.addNum(11);
-        medianFinder.addNum(6);
-        medianFinder.addNum(17);
-        medianFinder.addNum(14);
-        medianFinder.addNum(8);
-        medianFinder.addNum(17);
-        medianFinder.addNum(6);
-        medianFinder.addNum(4);
-        medianFinder.addNum(16);
-        medianFinder.addNum(8);
-        medianFinder.addNum(10);
-        double a = medianFinder.findMedian();
-        System.out.println(a);
+int[] a = {-2,1,-3,4,-1,2,1,-5,4};
+int aa = Solution.maxSubArray(a);
+System.out.println(aa);
     }
 
 }
