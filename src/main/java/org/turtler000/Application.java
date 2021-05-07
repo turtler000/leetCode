c029d95678dafbbf2ef11f45f3ee562816ae1ab2
hanhuanyu <hanhuanyu@inspur.com>
2021-05-07 16:30:10 +0800

    feat: 写简历

--- src/main/java/org/turtler000/Application.java
+++ src/main/java/org/turtler000/Application.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.turtler000.sword.package61to68.offer66.Solution;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 
 /**
@@ -18,8 +15,10 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
 
-List list = new ArrayList();
-list.add("123");
+Map map = new HashMap();
+map.put(new String("a"),"df");
+map.put(new String("a"),"dfa");
+List list = (List)map.get("a");
 System.out.println(list.get(0));
     }
 // 12 22 25 1225
